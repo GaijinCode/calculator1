511727c52c287729e2157fb213a3c7720db0cc25
GaijinCode <98213199+GaijinCode@users.noreply.github.com>
2022-07-23 17:07:23 +0300

    calc

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let clear = document.querySelector(".btn-clear");
+  let equal = document.querySelector(".btn-equal");
+
+  /* Adding number button functionality */
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      let value = e.target.dataset.num;
+      screen.value += value;
+    });
+  });
+
+  /*Making the "equal" button work */
+  equal.addEventListener("click", function (e) {
+    if (screen.value === "") {
+      screen.value = "";
+    } else {
+      let answer = eval(screen.value);
+      screen.value = answer;
+    }
+  });
+
+  /*Making the "clear" button work */
+  clear.addEventListener("click", function (e) {
+    screen.value = "";
+  });
+})();
